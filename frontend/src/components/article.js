e0616989321216087e17af6a2cd04077d13b5888
Piotr Konorski <piotr.konorski@gmail.com>
2022-03-28 21:58:26 +0200

    wip: table

--- frontend/src/components/article.js
+++ frontend/src/components/article.js
@@ -45,6 +45,44 @@ export default function Aricle(props) {
         </div>
       )} */}
 
+      {/* ************ */}
+
+
+      {/* <div class="accordion" id="accordionExample">
+        <div class="accordion-item bg-white border border-gray-200">
+          <h2 class="accordion-header mb-0" id="headingOne">
+            <button class="
+              accordion-button
+              relative
+              flex
+              items-center
+              w-full
+              py-4
+              px-5
+              text-base text-gray-800 text-left
+              bg-white
+              border-0
+              rounded-none
+              transition
+              focus:outline-none
+            " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+              aria-controls="collapseOne">
+              Найближчі аптеки
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body py-4 px-5">
+
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+
+
+      {/* ************ */}
+
       {content.map(({ id, header, subheader, type, content, omitSeparator }) => {
         return (
           <div key={id} id={id} className="max-w-3xl w-full">
